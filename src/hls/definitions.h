e1c7ac92163fa9d338376ec0a552c230a18fbd1f
gubbriaco <101352023+gubbriaco@users.noreply.github.com>
2024-06-09 16:02:30 +0200

    aggiunte size=8 e size=16

--- src/hls/definitions.h
+++ src/hls/definitions.h
@@ -4,17 +4,26 @@
 /**
  * Square Matrix Size.
  */
-const static int size = 4;
+//4
+//8
+//16
+const static int size = 8;
 
 /**
  * Number of Non-Zero Elements.
  */
-const static int noZeroEl  = 9;
+//9
+//13
+//21
+const static int noZeroEl  = 13;
 
 /**
  * Number of Rows.
  */
-const static int rows = 4;
+//4
+//8
+//16
+const static int rows = 8;
 
 /**
  * Data Type.
@@ -22,21 +31,21 @@ const static int rows = 4;
 typedef int DTYPE;
 
 /**
- * Sparse Matrix Multiplication Unoptimized Design.
+ * Standard Matrix Multiplication Design.
+ * @param matrix[size][size] Input matrix
+ * @param resMul Multiplication Result
+ * @param vector Input Vector
+ */
+void std_multiplication(int iFirstEl[rows+1], int iNonZeroEl[noZeroEl], DTYPE values[noZeroEl], DTYPE mulRes[size], DTYPE vector[size]);
+
+/**
+ * Sparse Matrix Multiplication Design.
  * @param iFirstEl[rows+1] Indexes First Elements
  * @param iNonZeroEl[noZeroEl] Indexes Non Zero Elements
  * @param values[noZeroEl] Input Values
  * @param mulRes[size] Multiplication Result
  * @param vector[size] Input Vector
  */
-void std_multiplication(DTYPE matrix[size][size], DTYPE *mulRes, DTYPE *vector);
-
-/**
- * Standard Matrix Multiplication Design.
- * @param matrix[size][size] Input matrix
- * @param resMul Multiplication Result
- * @param vector Input Vector
- */
-void smvm(int iFirstEl[rows+1], int iNonZeroEl[noZeroEl], DTYPE values[noZeroEl], DTYPE mulRes[size], DTYPE vector[size]);
+void smvm(DTYPE matrix[size][size], DTYPE *mulRes, DTYPE *vector);
 
 #endif
--- src/hls/smvmTB.cpp
+++ src/hls/smvmTB.cpp
@@ -2,29 +2,70 @@
 #include <iostream>
 using namespace std;
 
-
 void std_multiplication(DTYPE matrix[size][size], DTYPE *mulRes, DTYPE *vector);
-void smvm(int iFirstEl[rows+1], int iNonZeroEl[noZeroEl ], DTYPE values[noZeroEl], DTYPE mulRes[size], DTYPE vector[size]);
+void smvm(int iFirstEl[rows+1], int iNonZeroEl[noZeroEl], DTYPE values[noZeroEl], DTYPE mulRes[size], DTYPE vector[size]);
+
 
 int main() {
-
 	int fail = 0;
+
+/*
 	DTYPE matrix[size][size] = {
 			{3,4,0,0},
 			{0,5,9,0},
 			{2,0,3,1},
 			{0,4,0,6}
 	};
-	DTYPE vector[size] = {1,1,1,1};
-	DTYPE mulResSW[size];
-	DTYPE values[] = {3,4,5,9,2,3,1,4,6};
-	int iNonZeroEl[] = {0,1,1,2,0,2,3,1,3};
+	DTYPE vector[size] = {1, 1, 1, 1};
+	DTYPE values[] = {3, 4, 5, 9, 2, 3, 1, 4, 6};
+	int iNonZeroEl[] = {0, 1, 1, 2, 0, 2, 3, 1, 3};
 	int iFirstEl[] = {0,2,4,7,9};
+*/
 
+	DTYPE matrix[size][size] = {
+			{3, 4, 0, 0, 0, 0, 0, 0},
+			{0, 5, 9, 0, 0, 0, 0, 0},
+			{2, 0, 3, 1, 0, 0, 0, 0},
+			{0, 4, 0, 6, 0, 0, 0, 0},
+			{1, 0, 0, 0, 0, 0, 0, 0},
+			{1, 0, 0, 0, 0, 0, 0, 0},
+			{1, 0, 0, 0, 0, 0, 0, 0},
+			{1, 0, 0, 0, 0, 0, 0, 0}
+	};
+	DTYPE vector[size] = {1, 1, 1, 1, 1, 1, 1, 1};
+	DTYPE values[] = {3, 4, 5, 9, 2, 3, 1, 4, 6, 1, 1, 1, 1};
+	int iNonZeroEl[] = {0, 1, 1, 2, 0, 2, 3, 1, 3, 0, 0, 0, 0};
+	int iFirstEl[] = {0, 2, 4, 7, 9, 10, 11, 12, 13};
+
+/*
+	DTYPE matrix[size][size] = {
+				{3, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+				{0, 5, 9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+				{2, 0, 3, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+				{0, 4, 0, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+				{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+				{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+				{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+				{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+				{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+				{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+				{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+				{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+				{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+				{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+				{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+				{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+	};
+	DTYPE vector[size] = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
+	DTYPE values[] = {3, 4, 5, 9, 2, 3, 1, 4, 6, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
+	int iNonZeroEl[] = {0, 1, 1, 2, 0, 2, 3, 1, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+	int iFirstEl[] = {0, 2, 4, 7, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22};
+*/
+
+
+	DTYPE mulResSW[size];
 	std_multiplication(matrix, mulResSW, vector);
-
 	DTYPE mulRes[size];
-
 	smvm(iFirstEl, iNonZeroEl, values, mulRes, vector);
 	cout << endl;
 	cout << "*** Unoptimized Design ***" << endl;
@@ -38,9 +79,8 @@ int main() {
 		else
 			cout << "i=" << i << " passed." << endl;
 	}
-
 	cout << endl;
+
 	return 0;
 	//return fail;
-
 }
