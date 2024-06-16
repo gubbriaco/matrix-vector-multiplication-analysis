8e99bb7fe0e3f0d93fe989dedbeb69a0341b87fa
gubbriaco <101352023+gubbriaco@users.noreply.github.com>
2024-06-16 19:12:48 +0200

    report terminato

---
+++ report/solutions/s9/s9pipelineloop1rowPtry.cpp
@@ -0,0 +1,20 @@
+#include "definitions.h"
+
+void smvm(int rowPtr[rows+1], int columnIndex[nnz], DTYPE values[nnz], DTYPE y[size], DTYPE x[size]) {
+	loop1: for (int i=0; i<rows; i++) {
+		#pragma HLS pipeline
+		DTYPE ytmp = 0;
+		loop2: for (int k=rowPtr[i]; k<rowPtr[i+1]; k++) {
+			#pragma HLS loop_tripcount min=0 max=4 avg=2
+			#pragma HLS pipeline
+			#pragma HLS unroll factor=8
+			#pragma HLS array_partition variable=columnIndex complete
+			#pragma HLS array_partition variable=values complete
+			#pragma HLS array_partition variable=x complete
+			#pragma HLS array_partition variable=rowPtr complete
+			#pragma HLS array_partition variable=y complete
+			ytmp += values[k] * x[columnIndex[k]];
+		}
+		y[i] = ytmp;
+	}
+}
